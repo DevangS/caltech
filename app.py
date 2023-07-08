f6fc3d2890c699a95ff2b71709244948c3608091
Devang Sampat <devangs27@gmail.com>
2023-07-08 12:05:41 -0700

    add more error handling

--- app.py
+++ app.py
@@ -34,13 +34,17 @@ def get_plugshare(location_id=352230):
     print(s.cookies.get_dict())
 
 def get_chargers():
-    r = requests.get(
-        'https://caltech.powerflex.com/api/datasources/proxy/20/query?db=exload&q=SELECT%20last(%22mamps_last%22)%20%20%2F%201000%20AS%20%22Measured%22%2C%20last(%22mamps_offered%22)%20%20%2F%201000%20AS%20%22Allocated%22%2C%20last(%22SoC%22)%20*100%20AS%20%22SoC%22%2C%20last(%22current_energy_delivered%22)%20AS%20%22Current%20Energy%20Delivered%22%20FROM%20%22DCFC_Session%22%20WHERE%20(%22evse_type%22%20%3D%20%27Tritium%20DCFC%27%20AND%20%22space_number%22%20!%3D%20%27veefil-11900390%27%20AND%20%22space_number%22%20!%3D%20%27veefil-11900388%27)%20AND%20time%20%3E%3D%20now()%20-%2010m%20GROUP%20BY%20%22space_number%22&epoch=ms').json()
-    data = r['results'][0]['series']
-    for c in data:
-        result = dict(zip(c['columns'], c['values'][0]))
-        result['name'] = c['tags']['space_number']
-        yield result
+    try:
+        r = requests.get(
+            'https://caltech.powerflex.com/api/datasources/proxy/20/query?db=exload&q=SELECT%20last(%22mamps_last%22)%20%20%2F%201000%20AS%20%22Measured%22%2C%20last(%22mamps_offered%22)%20%20%2F%201000%20AS%20%22Allocated%22%2C%20last(%22SoC%22)%20*100%20AS%20%22SoC%22%2C%20last(%22current_energy_delivered%22)%20AS%20%22Current%20Energy%20Delivered%22%20FROM%20%22DCFC_Session%22%20WHERE%20(%22evse_type%22%20%3D%20%27Tritium%20DCFC%27%20AND%20%22space_number%22%20!%3D%20%27veefil-11900390%27%20AND%20%22space_number%22%20!%3D%20%27veefil-11900388%27)%20AND%20time%20%3E%3D%20now()%20-%2010m%20GROUP%20BY%20%22space_number%22&epoch=ms').json()
+        data = r['results'][0]['series']
+        for c in data:
+            result = dict(zip(c['columns'], c['values'][0]))
+            result['name'] = c['tags']['space_number']
+            yield result
+    except Exception as e:
+        print(e)
+        return []
 
 
 if __name__ == '__main__':
